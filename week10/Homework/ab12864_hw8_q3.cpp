39141ced5677a1b775948983d9b522e9a0d5b737
Archana Balachandran <archana.chali@gmail.com>
2024-11-09 15:07:29 +0530

    Q3 and math

---
+++ week10/Homework/ab12864_hw8_q3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int arrSize);
+void removeOdd(int arr[], int& arrSize);
+void splitParity(int arr[], int arrSize);
+int main(){
+
+}
+
+void reverseArray(int arr[], int arrSize){
+
+    
+}
