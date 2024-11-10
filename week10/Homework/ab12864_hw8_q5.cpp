796f269c9f6b08aed9bbc82675c0c53f52d52bcd
Archana Balachandran <archana.chali@gmail.com>
2024-11-10 14:10:45 +0530

    Q5

---
+++ week10/Homework/ab12864_hw8_q5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string temp, firstName, middleName, periodFollowingMiddleName, lastName;
+
+    cout<<"Please enter your name in the format - first name, middle name, last name: ";
+    cin>>firstName>>middleName>>lastName;
+
+    if (middleName[1] == '.'){
+        cout<<"User, "<<firstName<<" "<<middleName;
+    }
+    else{
+        cout<<"User, "<<firstName<<" "<<middleName[0]<<". ";
+    }
+    
+
+    return 0;
+}
