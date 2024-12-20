1c098aa5b01bad6437f7d9a3c530efb3e57c854c
Archana Balachandran <archana.chali@gmail.com>
2024-12-21 01:43:50 +0530

    hw12 submitted

--- week15/Homework/ab12864_hw12_q1.cpp
+++ week15/Homework/ab12864_hw12_q1.cpp
@@ -1,25 +1,3 @@
-// CH12 #7: Write a checkbook balancing program. The program will read in, from the
-// console, the following for all checks that were not cashed as of the last time you balanced your
-// checkbook:
-// - The number of each check (int)
-// - The amount of the check (double)
-// - Whether or not it has been cashed (1 or 0, boolean in the array)
-// Use an array with the class as the type. The class should be a class for a check. There
-// should be three member variables to record the check number, the check amount, and whether
-// or not the check was cashed. The class for a check will have a member variable of type Money
-// (as defined on page 662 in the book; Display 11.9) to record the check amount. So, you will
-// have a class used within a class. The class for a check should have accessor and mutator
-// functions as well as constructors and functions for both input and output of a check. In addition
-// to the checks, the program also reads all the deposits (from the console; cin), the old and the
-// new account balance (read this in from the user at the console; cin). You may want another
-// array to hold the deposits. The new account balance should be the old balance plus all deposits,
-// minus all checks that have been cashed.
-// The program outputs the total of the checks cashed, the total of the deposits, what the new
-// balance should be, and how much this figure differs from what the bank says the new balance
-// is. It also outputs two lists of checks: the checks cashed since the last time you balanced your
-// checkbook and the checks still not cashed. [edit: if you can, Display both lists of checks in
-// sorted order from lowest to highest check number.]
-
 #include <iostream>
 #include <cstdlib>
 #include <cctype>
@@ -30,42 +8,26 @@ using namespace std;
 class Money{
 public:
     friend Money operator +(const Money& amount1, const Money& amount2);
-    //Returns the sum of the values of amount1 and amount2.
 
     friend Money operator -(const Money& amount1, const Money& amount2);
-    //Returns amount 1 minus amount2.
 
     friend Money operator -(const Money& amount);
-    //Returns the negative of the value of amount.
 
     friend bool operator ==(const Money& amount1, const Money& amount2);
-    //Returns true if amount1 and amount2 have the same value; false otherwise.
 
     friend bool operator < (const Money& amount1, const Money& amount2);
-    //Returns true if amount1 is less than amount2; false otherwise.
 
     Money(long dollars, int cents);
-    //Initializes the object so its value represents an amount with
-    //the dollars and cents given by the arguments. If the amount
-    //is negative, then both dollars and cents should be negative.
 
     Money(long dollars);
-    //Initializes the object so its value represents $dollars.00.
 
     Money( );
-    //Initializes the object so its value represents $0.00.
 
     double get_value( ) const;
-    //Returns the amount of money recorded in the data portion of the calling
-    //object.
 
     friend istream& operator >>(istream& ins, Money& amount);
-    //Overloads the >> operator so it can be used to input values of type
-    //Money. Notation for inputting negative amounts is as in -$100.00.
 
     friend ostream& operator <<(ostream& outs, const Money& amount);
-    //Overloads the << operator so it can be used to output values of type
-    //Money. Precedes each output value of type Money with a dollar sign.
 
 private:
     long all_cents;
@@ -75,36 +37,20 @@ private:
 class Check{
     public:
         Check(int number, Money amount, bool isCashed);
-        //Initializes the object so its value represents a check with
-        //the checkNumber = number, checkAmount = amount and checkCashed = isCashed
-
-        //Check(int number, long dollars, bool cashed);
-        //Initializes the object so its value represents a check with
-        //the checkNumber = number, checkAmount = $dollar.00 and checkCashed = isCashed
 
         Check();
-        //Initializes the object so its value represents a check with
-        //the checkNumber = 0, checkAmount = 0.0 given by Money() and checkCashed = false;
 
         int getNumber() const;
-        //Returns the check number recorded in the data portion of the calling object
 
         Money getAmount() const;
-        //Returns the amount recorded in the data portion of the calling object.
 
         bool getIsCashed() const;
-        //Returns the cashing status recorded in the data portion of the calling object.
+
+        void set(int number, Money amount, bool isCashed);
 
         friend istream& operator >>(istream& ins, Check& currCheck);
-        //Overloads the >> operator so it can be used to input values of type
-        //Check. Notation for inputting negative amounts is as in -$100.00.
 
         friend ostream& operator <<(ostream& outs, const Check& currCheck);
-        //Overloads the << operator so it can be used to output values of type
-        //Check. Precedes each output value of type Money with a dollar sign.
-
-        friend bool operator >(const Check& checkLeft, const Check& checkRight);
-        //Returns true if the check number of the check on the left side is greater than the one on the right
 
     private:
         int checkNumber;
@@ -114,7 +60,7 @@ class Check{
 
 //Other function declarations
 int digit_to_int(char c);
-void sort(Check userChecks[], int numOfChecks);
+void sort(Check* userChecks, int numOfChecks);
 
 int main( ){
 
@@ -126,7 +72,9 @@ int main( ){
     
     Check* userChecks = new Check[numOfChecks];
 
-    cout<<"For each check, enter the checkNumber (positive int) space checkAmount (non-negative double) space checkCashStatus (y or n): ";
+    if (numOfChecks>0){
+        cout<<"For each check, enter the check number, amount and encashment status (Eg: 1 $ab.cd y): "<<endl;
+    }
     for (int i = 0; i < numOfChecks; i++){
         cin>>userChecks[i];
         if (userChecks[i].getIsCashed()){
@@ -136,31 +84,59 @@ int main( ){
             sumUncashedChecks = sumUncashedChecks + userChecks[i].getAmount();
         }
     }
+    if (numOfChecks == 0){
+        sumCashedChecks = Money();
+        sumUncashedChecks = Money();
+    }
 
+    cout<<endl;
     cout<<"Please enter the number of deposits you've made: ";
     cin>>numOfDeposits;
 
     Money* userDeposits = new Money[numOfDeposits];
 
-    cout<<"For each deposit, enter the deposit amount in the for $ab.cd, separated by spaces: ";
+    if (numOfDeposits>0){
+        cout<<"For each deposit, enter deposit amounts in the form $ab.cd, each in a new line: "<<endl;
+    }
     for (int i = 0; i < numOfDeposits; i++){
         cin>>userDeposits[i];
         sumUserDeposits = sumUserDeposits + userDeposits[i];
     }
+    if (numOfDeposits==0){
+        sumUserDeposits = Money();
+    }
 
-    cout<<"Please enter your old balance in the form $ab.cd: ";
+    cout<<endl;
+    cout<<"Please enter your old balance in the form $ab.cd: "<<endl;
     cin>>oldBalance;
 
     newBalance = oldBalance + sumUserDeposits - sumCashedChecks;
 
+    cout<<endl;
     cout<<"Total checks cashed is: "<<sumCashedChecks<<endl;
     cout<<"Total deposits is: "<<sumUserDeposits<<endl;
     cout<<"New balance is: "<<newBalance<<endl;
     cout<<"After accounting for uncashed checks, the balance will be: "<<newBalance-sumUncashedChecks<<endl;
     cout<<"The difference is: "<<sumUncashedChecks<<endl;
 
+    cout<<endl;
+    sort(userChecks, numOfChecks);
+    if (numOfChecks>0){
+        cout<<"Cashed checks in order of check number"<<endl;
+    }
     for (int i = 0; i <numOfChecks; i++){
-        cout<<userChecks[i];
+        if (userChecks[i].getIsCashed()){
+            cout<<userChecks[i];
+        }    
+    }
+    cout<<endl;
+    if (numOfChecks>0){
+        cout<<"Uncashed checks in order of check number"<<endl;
+    }
+    for (int i = 0; i <numOfChecks; i++){
+        if (!userChecks[i].getIsCashed()){
+            cout<<userChecks[i];
+        }    
     }
 
     delete [] userChecks;
@@ -176,8 +152,7 @@ Money operator +(const Money& amount1, const Money& amount2){
     return temp;
 }
 
-Money operator -(const Money& amount1, const Money& amount2)
-{
+Money operator -(const Money& amount1, const Money& amount2){
     Money temp;
     temp.all_cents = amount1.all_cents - amount2.all_cents;
     return temp;
@@ -209,13 +184,11 @@ Money::Money(long dollars) : all_cents(dollars*100){}
 
 Money::Money( ) : all_cents(0){}
 
-double Money::get_value( ) const
-{
+double Money::get_value( ) const{
     return (all_cents * 0.01);
 }
 
-istream& operator >>(istream& ins, Money& amount)
-{
+istream& operator >>(istream& ins, Money& amount){
     char one_char, decimal_point,
          digit1, digit2; //digits for the amount of cents
     long dollars;
@@ -250,8 +223,7 @@ istream& operator >>(istream& ins, Money& amount)
     return ins;
 }
 
-ostream& operator <<(ostream& outs, const Money& amount)
-{
+ostream& operator <<(ostream& outs, const Money& amount){
     long positive_cents, dollars, cents;
     positive_cents = labs(amount.all_cents);
     dollars = positive_cents/100;
@@ -284,6 +256,12 @@ Money Check::getAmount() const{
 
 bool Check::getIsCashed() const{
     return checkIsCashed;
+}
+
+void Check::set(int number, Money amount, bool isCashed){
+    checkNumber = number;
+    checkAmount = amount;
+    checkIsCashed = isCashed;
 }
 
 istream& operator >>(istream& ins, Check& currCheck){
@@ -324,14 +302,23 @@ ostream& operator <<(ostream& outs, const Check& currCheck){
     return outs;
 }
 
-bool operator >(const Check& checkLeft, const Check& checkRight){
-    return (checkLeft.checkNumber < checkRight.checkNumber);
-}
-
 //Additional functions
 int digit_to_int(char c){
     return ( static_cast<int>(c) - static_cast<int>('0') );
 }
 
-void sort(Check userChecks[], int numOfChecks){}
+void sort(Check* userChecks, int numOfChecks){
+
+    Check temp; 
+
+    for (int i = 0; i < numOfChecks - 1; i++){
+        for (int j = 0; j < numOfChecks-i-1; j++){
+            if ((*(userChecks+j)).getNumber() > (*(userChecks+j+1)).getNumber()){
+                temp = *(userChecks+j);
+                *(userChecks+j) = *(userChecks+j+1);
+                *(userChecks+j+1) = temp;
+            }
+        }
+    }
+}
 
