89aed6d0ff0912085d651e06e8cf96fbc3a66e6f
Archana Balachandran <archana.chali@gmail.com>
2025-01-11 02:37:45 +0530

    implementations v1

--- week15/Homework/ab12864_hw13_q1.cpp
+++ week15/Homework/ab12864_hw13_q1.cpp
@@ -2,14 +2,16 @@
 #include <cstdlib>
 #include <vector>
 #include <ctime>
+#include <typeinfo>
+
 using namespace std;
+
+const int GRID_SIZE = 20;
 
 //character denotation
 const char ANT_CHAR = 'o';
 const char DOODLEBUG_CHAR = 'X';
 const char SPACE_CHAR = '-';
-
-const int GRID_SIZE = 20;
 
 //start number of critters
 const int START_ANTS = 100, START_DOODLEBUGS = 5;
@@ -24,14 +26,26 @@ const int DOODLEBUG_STARVE_MOVE = 3;
 const int ANT_TYPE = 1;
 const int DOODLEBUG_TYPE = 2;
 
+//ints for move direction
+const int DEATH = -1;
+const int UNCHANGED = 0;
+const int UP = 1;
+const int DOWN = 2;
+const int LEFT = 3;
+const int RIGHT = 4;
+
+class Universe;
+
+//class Organism
 class Organism{
     public:
         Organism();
+        Organism(int initial_row, int initial_col);
         int get_row() const;
         void set_row(int the_row);
         int get_col() const;
         void set_col(int the_col);
-        virtual void move();
+        virtual void move(Universe& the_universe, int direction) = 0;
         virtual int get_type() = 0;
     private:
         int row;
@@ -42,18 +56,23 @@ class Universe{
     public:
         Universe();
         void initialize_universe();
-        //void simulate_time_step();
-        //bool check_space_empty(Organism arr[], int the_index);
+        void simulate_time_step();
+        int find_move_direction(int row, int col);
         void display_grid();
+        Organism* get_grid();
     private:
         Organism* grid[GRID_SIZE][GRID_SIZE];
         int num_of_timesteps;
+    friend class Doodlebug;
+    friend class Ant;
+
 };
 
 class Ant: public Organism{
     public:
         Ant();
-        virtual void move();
+        Ant(int initial_row, int initial_col);
+        virtual void move(Universe& the_universe, int direction);
         void breed();
         virtual int get_type();
 };
@@ -61,11 +80,15 @@ class Ant: public Organism{
 class Doodlebug: public Organism{
     public:
         Doodlebug();
-        virtual void move();
-        void breed();
-        void starve();
+        Doodlebug(int initial_row, int initial_col);
+        virtual void move(Universe& the_universe, int direction);
         void eat();
+        void breed();
         virtual int get_type();
+        int get_starve_counter();
+        void set_starve_counter(int current_count);
+    private:
+        int starve_counter;
 };
 
 int main(){
@@ -73,6 +96,12 @@ int main(){
     Universe uni1;
     uni1.initialize_universe();
     uni1.display_grid();
+    uni1.simulate_time_step();
+    cout<<endl;
+    uni1.display_grid();
+
+    //Ant ant1(4,6);
+    //cout<<ant1.get_row()<<endl;
 
     return 0;
 }
@@ -99,7 +128,7 @@ void Universe::initialize_universe(){
             row = rand()%20;
             col = rand()%20;
         }
-        grid[row][col] = new Ant;
+        grid[row][col] = new Ant(row, col);
     }
 
     for (int i = 0; i < START_DOODLEBUGS; i++){
@@ -111,8 +140,94 @@ void Universe::initialize_universe(){
             row = rand()%20;
             col = rand()%20;
         }
-        grid[row][col] = new Doodlebug;
+        grid[row][col] = new Doodlebug(row, col);
     }
+}
+
+void Universe::simulate_time_step(){
+    for (int i = 0; i < GRID_SIZE; i++){
+        for (int j = 0; j < GRID_SIZE; j++){
+            if ((grid[i][j] != nullptr) && (grid[i][j]->get_type() == DOODLEBUG_TYPE)){
+                int direction = find_move_direction(i,j);
+                Doodlebug* doodlebug = dynamic_cast<Doodlebug*>(grid[i][j]);
+                doodlebug -> move(*this, direction);
+            }
+        }
+    }
+    for (int i = 0; i < GRID_SIZE; i++){
+        for (int j = 0; j < GRID_SIZE; j++){
+            if ((grid[i][j] != nullptr) && (grid[i][j]->get_type() == ANT_TYPE)){
+                int direction = find_move_direction(i,j);
+                Ant* ant = dynamic_cast<Ant*>(grid[i][j]);
+                ant -> move(*this, direction);
+            }
+        }
+    }
+}
+
+int Universe::find_move_direction(int row, int col){
+    
+    vector <int> viable_positions;
+    srand(time(0));
+
+    if (grid[row][col]->get_type() == ANT_TYPE){
+        
+        if ((col > 0) && (grid[row][col-1] == nullptr)){
+            viable_positions.push_back(LEFT);
+        }
+        if ((col < GRID_SIZE-1) && (grid[row][col+1] == nullptr)){
+            viable_positions.push_back(RIGHT);
+        }
+        if ((row > 0) && (grid[row-1][col] == nullptr)){
+            viable_positions.push_back(UP);
+        }
+        if ((row < GRID_SIZE-1) && (grid[row+1][col] == nullptr)){
+            viable_positions.push_back(DOWN);
+        }
+    }
+
+    else if (grid[row][col]->get_type() == DOODLEBUG_TYPE){
+        if ((col > 0) && (grid[row][col-1] != nullptr) && (grid[row][col-1]->get_type() == ANT_TYPE)){
+            viable_positions.push_back(LEFT);
+        }
+        if ((col < GRID_SIZE-1) && (grid[row][col+1] != nullptr) && (grid[row][col+1]->get_type() == ANT_TYPE)){
+            viable_positions.push_back(RIGHT);
+        }
+        if ((row > 0) && (grid[row-1][col] != nullptr) && (grid[row-1][col]->get_type() == ANT_TYPE)){
+            viable_positions.push_back(UP);
+        }
+        if ((row < GRID_SIZE-1) && (grid[row+1][col] != nullptr) && (grid[row+1][col]->get_type() == ANT_TYPE)){
+            viable_positions.push_back(DOWN);
+        }
+
+        Doodlebug* doodlebug = dynamic_cast<Doodlebug*>(grid[row][col]);
+
+        if (viable_positions.empty()){
+            if (doodlebug->get_starve_counter() == 3){
+                return DEATH;
+            }
+            if ((col > 0) && (grid[row][col-1] == nullptr)){
+            viable_positions.push_back(LEFT);
+            }
+            if ((col < GRID_SIZE-1) && (grid[row][col+1] == nullptr)){
+                viable_positions.push_back(RIGHT);
+            }
+            if ((row > 0) && (grid[row-1][col] == nullptr)){
+                viable_positions.push_back(UP);
+            }
+            if ((row < GRID_SIZE-1) && (grid[row+1][col] == nullptr)){
+                viable_positions.push_back(DOWN);
+            }
+        }
+    }
+
+    if (viable_positions.empty()){
+        return UNCHANGED;
+    }
+    else{
+        return viable_positions[rand()%viable_positions.size()];
+    }
+    
 }
 
 void Universe::display_grid(){
@@ -135,6 +250,8 @@ void Universe::display_grid(){
 //Organism class implementation
 Organism::Organism():row(-1), col(-1){}
 
+Organism::Organism(int initial_row, int initial_col):row(initial_row), col(initial_col){}
+
 int Organism::get_row() const{
     return row;
 }
@@ -151,15 +268,36 @@ void Organism::set_col(int the_col){
     col = the_col;
 }
 
-void Organism::move(){
-    cout<<"Org moved ";
-}
-
 //Ant class implementation
 Ant::Ant():Organism(){}
 
-void Ant::move(){
-    cout<<"Ant moved ";
+Ant::Ant(int initial_row, int initial_col): Organism(initial_row, initial_col){}
+
+void Ant::move(Universe& the_universe, int direction){
+
+    int i = get_row();
+    int j = get_col();
+
+    if (direction == 1){
+        the_universe.grid[i-1][j] = the_universe.grid[i][j];
+        the_universe.grid[i-1][j]->set_row(i-1);
+        the_universe.grid[i][j] = nullptr;
+    }
+    else if (direction == 2){
+        the_universe.grid[i+1][j] = the_universe.grid[i][j];
+        the_universe.grid[i+1][j]->set_row(i+1);
+        the_universe.grid[i][j] = nullptr;
+    }
+    else if (direction == 3){
+        the_universe.grid[i][j-1] = the_universe.grid[i][j];
+        the_universe.grid[i][j-1]->set_col(j-1);
+        the_universe.grid[i][j] = nullptr;
+    }
+    else if (direction == 4){
+        the_universe.grid[i][j+1] = the_universe.grid[i][j];
+        the_universe.grid[i][j+1]->set_col(j+1);
+        the_universe.grid[i][j] = nullptr;
+    }
 }
 
 int Ant::get_type(){
@@ -169,10 +307,92 @@ int Ant::get_type(){
 //Doodlebug class implementation
 Doodlebug::Doodlebug():Organism(){}
 
-void Doodlebug::move(){
-    cout<<"Dbug moved ";
+Doodlebug::Doodlebug(int initial_row, int initial_col): Organism(initial_row, initial_col){}
+
+void Doodlebug::move(Universe& the_universe, int direction){
+
+    int i = get_row();
+    int j = get_col();
+
+
+    if (direction == 1){
+        if (the_universe.grid[i-1][j] == nullptr){
+            the_universe.grid[i-1][j] = the_universe.grid[i][j];
+            the_universe.grid[i-1][j]->set_row(i-1);
+            the_universe.grid[i][j] = nullptr;
+            Doodlebug* doodlebug = dynamic_cast<Doodlebug*>(the_universe.grid[i-1][j]);
+            doodlebug->set_starve_counter(doodlebug->get_starve_counter()+1);
+        }
+        else{
+            the_universe.grid[i-1][j] = nullptr;
+            the_universe.grid[i-1][j] = the_universe.grid[i][j];
+            the_universe.grid[i-1][j]->set_row(i-1);
+            the_universe.grid[i][j] = nullptr;
+            Doodlebug* doodlebug = dynamic_cast<Doodlebug*>(the_universe.grid[i-1][j]);
+            doodlebug->set_starve_counter(0);
+        }
+    }
+    else if (direction == 2){
+        if (the_universe.grid[i+1][j] == nullptr){
+            the_universe.grid[i+1][j] = the_universe.grid[i][j];
+            the_universe.grid[i+1][j]->set_row(i+1);
+            the_universe.grid[i][j] = nullptr;
+            Doodlebug* doodlebug = dynamic_cast<Doodlebug*>(the_universe.grid[i+1][j]);
+            doodlebug->set_starve_counter(doodlebug->get_starve_counter()+1);
+        }
+        else{
+            the_universe.grid[i+1][j] = nullptr;
+            the_universe.grid[i+1][j] = the_universe.grid[i][j];
+            the_universe.grid[i+1][j]->set_row(i+1);
+            the_universe.grid[i][j] = nullptr;
+            Doodlebug* doodlebug = dynamic_cast<Doodlebug*>(the_universe.grid[i+1][j]);
+            doodlebug->set_starve_counter(0);
+        }
+    }
+    else if (direction == 3){
+        if (the_universe.grid[i][j-1] == nullptr){
+            the_universe.grid[i][j-1] = the_universe.grid[i][j];
+            the_universe.grid[i][j-1]->set_col(j-1);
+            the_universe.grid[i][j] = nullptr;
+            Doodlebug* doodlebug = dynamic_cast<Doodlebug*>(the_universe.grid[i][j-1]);
+            doodlebug->set_starve_counter(doodlebug->get_starve_counter()+1);
+        }
+        else{
+            the_universe.grid[i][j-1] = nullptr;
+            the_universe.grid[i][j-1] = the_universe.grid[i][j];
+            the_universe.grid[i][j-1]->set_col(j-1);
+            the_universe.grid[i][j] = nullptr;
+            Doodlebug* doodlebug = dynamic_cast<Doodlebug*>(the_universe.grid[i][j-1]);
+            doodlebug->set_starve_counter(0);
+        }
+    }
+    else if (direction == 4){
+        if (the_universe.grid[i][j+1] == nullptr){
+            the_universe.grid[i][j+1] = the_universe.grid[i][j];
+            the_universe.grid[i][j+1]->set_col(j+1);
+            the_universe.grid[i][j] = nullptr;
+            Doodlebug* doodlebug = dynamic_cast<Doodlebug*>(the_universe.grid[i][j+1]);
+            doodlebug->set_starve_counter(doodlebug->get_starve_counter()+1);
+        }
+        else{
+            the_universe.grid[i][j+1] = nullptr;
+            the_universe.grid[i][j+1] = the_universe.grid[i][j];
+            the_universe.grid[i][j+1]->set_col(j+1);
+            the_universe.grid[i][j] = nullptr;
+            Doodlebug* doodlebug = dynamic_cast<Doodlebug*>(the_universe.grid[i][j+1]);
+            doodlebug->set_starve_counter(0);
+        }
+    }
 }
 
 int Doodlebug::get_type(){
     return DOODLEBUG_TYPE;
+}
+
+int Doodlebug::get_starve_counter(){
+    return starve_counter;
+}
+
+void Doodlebug::set_starve_counter(int current_count){
+    starve_counter= current_count;
 }
