c0ff3592bbcd2e38a182cb981fd1a71bc9d898c4
Archana Balachandran <archana.chali@gmail.com>
2025-01-24 18:54:32 +0530

    first entry read into head node

--- week18/Homework/ab12864_hw15_q1.cpp
+++ week18/Homework/ab12864_hw15_q1.cpp
@@ -10,19 +10,20 @@ class Account
 {
 private:
     string name;
-    int amount_paid;
-    int amount_owed;
+    double amount_paid;
+    double amount_owed;
     bool paid_status;
     Account* link;
 public:
     Account();
-    Account(string the_name, int the_amount_paid, int the_amount_owed, bool the_paid_status, Account* the_next);
+    Account(string the_name, double the_amount_paid);
+    Account(string the_name, double the_amount_paid, double the_amount_owed, bool the_paid_status, Account* the_next);
     string get_name() const;
     void set_name(string the_name);
-    int get_amount_paid() const;
-    void set_amount_paid(int the_amount_paid);
-    int get_amount_owed() const;
-    void set_amount_owed(int the_amount_owed);
+    double get_amount_paid() const;
+    void set_amount_paid(double the_amount_paid);
+    double get_amount_owed() const;
+    void set_amount_owed(double the_amount_owed);
     bool get_paid_status() const;
     void set_paid_status(bool the_paid_status);
     Account* get_link() const;
@@ -31,7 +32,7 @@ public:
 
 typedef Account* AccountNodePtr;
 
-void head_insert(AccountNodePtr& the_head, string the_name, int the_amount_paid, int the_amount_owed, bool the_paid_status, Account* the_next);
+void head_insert(AccountNodePtr& the_head, string the_name, double the_amount_paid, double the_amount_owed, bool the_paid_status, Account* the_next);
 
 int main(){
 
@@ -39,19 +40,18 @@ int main(){
     char character;
     double amount;
     ifstream in_stream;
-    AccountNodePtr head;
+    AccountNodePtr head = new Account;
 
     cout<<"Enter the file name: ";
     cin>>in_file_name;
 
     in_stream.open(in_file_name);
-    
     if (in_stream.fail()){
         cout<<"Input file opening failed.\n";
         exit(1);
     }
 
-
+    //first entry
     in_stream>>amount;
     in_stream.get(character);
 
@@ -59,9 +59,14 @@ int main(){
         name += character;
         in_stream.get(character);
     }
+
+    head -> set_name(name);
+    head -> set_amount_paid(amount);
+    head -> set_link(NULL);
     
-    cout<<"Amount: "<<amount<<endl;
-    cout<<"Name: "<<name<<endl;
+    cout<<"Amount: "<<head->get_amount_paid()<<endl;
+    cout<<"Name: "<<head->get_name()<<endl;
+    cout<<"Link: "<<head->get_link()<<endl;
         
     // while (!in_stream.eof()){
 
@@ -88,7 +93,9 @@ int main(){
 
 Account::Account(): name(""), amount_paid(0), amount_owed(0), paid_status(false), link(nullptr){}
 
-Account::Account(string the_name, int the_amount_paid, int the_amount_owed, bool the_paid_status, Account* the_next): name(the_name), amount_paid(the_amount_paid), amount_owed(the_amount_owed), paid_status(the_paid_status), link(the_next){}
+Account::Account(string the_name, double the_amount_paid):name(the_name), amount_paid(the_amount_paid){}
+
+Account::Account(string the_name, double the_amount_paid, double the_amount_owed, bool the_paid_status, Account* the_next): name(the_name), amount_paid(the_amount_paid), amount_owed(the_amount_owed), paid_status(the_paid_status), link(the_next){}
 
 string Account::get_name() const{
     return name;
@@ -98,19 +105,19 @@ void Account::set_name(string the_name){
     name = the_name;
 }
 
-int Account::get_amount_paid() const{
+double Account::get_amount_paid() const{
     return amount_paid;
 }
 
-void Account::set_amount_paid(int the_amount_paid){
+void Account::set_amount_paid(double the_amount_paid){
     amount_paid = the_amount_paid;
 }
 
-int Account::get_amount_owed() const{
+double Account::get_amount_owed() const{
     return amount_owed;
 }
 
-void Account::set_amount_owed(int the_amount_owed){
+void Account::set_amount_owed(double the_amount_owed){
     amount_owed = the_amount_owed;
 }
 
@@ -130,7 +137,7 @@ void Account::set_link(Account* the_next){
     link = the_next;
 }
 
-void head_insert(AccountNodePtr& the_head, string the_name, int the_amount_paid, int the_amount_owed, bool the_paid_status, Account* the_next){
+void head_insert(AccountNodePtr& the_head, string the_name, double the_amount_paid, double the_amount_owed, bool the_paid_status, Account* the_next){
     AccountNodePtr temp_ptr;
     temp_ptr = new Account(the_name, the_amount_paid, the_amount_owed, the_paid_status, the_head);
     the_head = temp_ptr;
